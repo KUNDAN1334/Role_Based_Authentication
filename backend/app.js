0da5368349d5a489718d60d8dae175162c3c913a
KUNDAN1334 <kundan.22bcon1334@jecrcu.edu.in>
2024-08-14 17:38:14 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/authentication");
+        await mongoose.connect("/*Your MONGODB STRING */");
         console.log("Connected to DB");
     } catch (error) {
         mongoose.disconnect();
